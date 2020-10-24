6c834a5c8daa64c1bdb8c12350018193edbefa85
Alysson Eduardo <alysson6801@hotmail.com>
2020-10-24 10:40:59 -0300

    (24/10) Grid de Gifs.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -83,6 +83,18 @@ class _HomePageState extends State<HomePage> {
     );
   }
   Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
-
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0
+        ),
+        itemCount: snapshot.data["data"].length,
+        itemBuilder: (context, index){
+          return GestureDetector(
+            child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"]),
+          );
+        }
+    );
   }
 }
